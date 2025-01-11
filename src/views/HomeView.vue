2ac21c1ffd766c61d29e3d81f2c02df653f16036
Roxana <ms.roxanarahimi@gmail.com>
2025-01-11 22:50:29 +0330

    first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,434 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="w-100 min-vh-100 d-none d-lg-grid text-center bg">
+    <div class="row p-0 m-0 h-100">
+      <div class="col-8 ps-5 h-100 d-grid">
+        <div class="ps-5 align-self-center">
+          <div class="row ps-5">
+            <div class="text-light col-4 d-grid ps-5 " style="font-family: 'YekanBakhExtraBlack';">
+              <h1 class="align-self-end">خـریــــد مستقیم</h1>
+            </div>
+            <div class="col-8">
+              <h1 class="text-light text-center mx-auto mt-5" style="font-size: 55px; text-align: justify">
+                بــرای شـرکــــت در مســـابقـه<br>
+                وارد گردونـه شـانس شـوید<br>
+                و کد تخفیف دریافـت کنید!</h1>
+            </div>
+            <div class="col-4 mx-auto px-2 mt-4">
+
+
+              <a
+                  href="https://www.okala.com/search?search_text=%D8%B9%D8%B5%D8%A7%D8%B1%D9%87%20%D8%A7%D9%84%DB%8C%D8%AA&sort=7"
+                  target="_blank" class="mb-3 digikala button  rounded rounded-4 d-flex justify-content-center"
+                  title="اُکالا">
+                <img width="60" height="80" src="/img/okala.svg" class="align-self-center" alt="اُکالا">
+              </a>
+              <a
+                  href="https://express.snapp.market/search-result?query=%D8%B9%D8%B5%D8%A7%D8%B1%D9%87%20%D8%A7%D9%84%DB%8C%D8%AA"
+                  target="_blank" class="mb-3 snappEx button rounded rounded-4 d-flex justify-content-center"
+                  title="اسنپ اکسپرس">
+                <img width="100" height="80" src="/img/snappEx.svg" class="align-self-center" alt="اسنپ اکسپرس">
+
+              </a>
+              <a
+                  href="https://www.digikala.com/search/food-beverage/?q=%D8%B9%D8%B5%D8%A7%D8%B1%D9%87%20%D8%A7%D9%84%DB%8C%D8%AA"
+                  target="_blank" class="mb-2 digikala button  rounded rounded-4 d-flex justify-content-center"
+                  title="دیجی کالا">
+                <img width="100" height="80" src="/img/digikala.svg" class="align-self-center" alt="دیجی کالا">
+              </a>
+            </div>
+            <div class="col-8 d-flex">
+              <a data-bs-toggle="modal" data-bs-target="#staticBackdrop"
+                 class="w-100 cursor-pointer text-decoration-none text-light mt-4 mb-2 play-btn button rounded rounded-4 d-grid justify-content-center"
+                 title="گردونه شانس">
+                <div class="position-relative chance">
+                  <img src="/img/gb.png" class="img-fluid mx-auto mt-3 wheel" width="180"
+                       alt="">
+                  <img src="/img/bf.png" class="img-fluid mx-auto "
+                       style="position: absolute; top:0px; left:90px"
+                       width="47" alt="">
+                </div>
+
+                <h1 class="align-self-center mx-auto">گردونه شانس!</h1>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-4 h-100 py-5 text-center">
+        <img src="/img/logo.png" class="d-block mx-auto" width="150" alt="">
+        <img src="/img/txt.png" class="d-block mx-auto" width="400" alt="">
+        <div class="text-center d-flex justify-content-center">
+          <div class="text-center" style="width: 400px">
+            <img src="/img/pic.png" class="mx-auto w-100" alt="">
+          </div>
+        </div>
+        <div class="text-light d-inline-grid">
+          <p class="align-self-center fs-3 fw-bold" style="font-family: YekanBakh">
+            manoelite
+            <i class="bi bi-instagram ms-1 fs-3 align-self-center"></i>
+          </p>
+
+        </div>
+      </div>
+
+    </div>
   </div>
+  <div class="w-100 min-vh-100 d-grid d-lg-none text-center bgM">
+    <div class="row p-0 m-0 align-self-stretch ">
+      <div class="col-12 mainMob pt-5 text-center">
+        <div class="row pt-3">
+          <div class="col-4 d-grid h-100 ps-1 pe-0">
+            <img src="/img/logo.png" class="d-block mx-auto align-self-center mb-3" width="100" alt="">
+            <img src="/img/txt.png" class="d-block mx-auto align-self-center" width="100%" alt="">
+            <div class="text-light d-inline-grid fs-5">
+              <p class="align-self-center fw-bold" style="font-family: YekanBakh">
+                manoelite
+                <i class="bi bi-instagram ms-1 align-self-center"></i>
+              </p>
+            </div>
+          </div>
+          <div class="col-8 ps-0 pe-1">
+            <div class="text-center d-grid">
+              <img src="/img/pic.png" class="mx-auto align-self-center w-100" alt="">
+            </div>
+
+          </div>
+        </div>
+
+
+      </div>
+      <div class="col-12 main1Mob d-grid pb-3">
+        <div class="align-self-center">
+          <div class="row px-2">
+            <div class="row p-0 m-0 text-light mt-4 mb-3  text-start" style="font-family: 'YekanBakhExtraBlack';">
+
+              <div class="col-4 ps-4 d-grid">
+                <h1 class="align-self-end">خــریـــــــد مستقیم</h1>
+              </div>
+              <div class="col-8">
+                <h4 class="mx-auto ps-2">
+                  برای شــــرکـت در مسـابقـــه<br>
+                  وارد گردونه شـانس شـوید<br>
+                  و کد تخفیف دریافت کنید!</h4>
+              </div>
+            </div>
+
+            <div class="col-4 mx-auto px-2">
+              <a
+                  href="https://www.okala.com/search?search_text=%D8%B9%D8%B5%D8%A7%D8%B1%D9%87%20%D8%A7%D9%84%DB%8C%D8%AA&sort=7"
+                  target="_blank" class="mb-2 digikala button  rounded rounded-4 d-flex justify-content-center"
+                  title="اُکالا">
+                <img width="45" height="80" src="/img/okala.svg" class="align-self-center" alt="اُکالا">
+              </a>
+              <a
+                  href="https://express.snapp.market/search-result?query=%D8%B9%D8%B5%D8%A7%D8%B1%D9%87%20%D8%A7%D9%84%DB%8C%D8%AA"
+                  target="_blank" class="mb-2 snappEx button rounded rounded-4 d-flex justify-content-center"
+                  title="اسنپ اکسپرس">
+                <img width="80" height="80" src="/img/snappEx.svg" class="align-self-center" alt="اسنپ اکسپرس">
+              </a>
+              <a
+                  href="https://www.digikala.com/search/food-beverage/?q=%D8%B9%D8%B5%D8%A7%D8%B1%D9%87%20%D8%A7%D9%84%DB%8C%D8%AA"
+                  target="_blank" class="mb-2 digikala button  rounded rounded-4 d-flex justify-content-center"
+                  title="دیجی کالا">
+                <img width="80" height="80" src="/img/digikala.svg" class="align-self-center" alt="دیجی کالا">
+              </a>
+
+
+            </div>
+            <div class="col-8 d-flex">
+
+              <a data-bs-toggle="modal" data-bs-target="#staticBackdrop"
+                 class="w-100  text-decoration-none text-light  mb-2 play-btn button rounded rounded-4 d-grid justify-content-center"
+                 title="گردونه شانس">
+                <div class="position-relative chance">
+                  <img src="/img/gb.png" class="img-fluid mx-auto mt-3 wheel" width="200"
+                       alt="">
+                  <img src="/img/bf.png" class="img-fluid mx-auto "
+                       style="position: absolute; top:5px; left:75px"
+                       width="47" alt="">
+                </div>
+
+                <h1 class="align-self-center mx-auto">گردونه شانس!</h1>
+
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Modal -->
+  <div class="modal fade pb-4 " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+       aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header border-0">
+          <button type="button" class="btn-close px-0 m-0" data-bs-dismiss="modal" aria-label="Close"></button>
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">گردونه شانس</h1>
+        </div>
+
+        <div class="modal-body pt-0" style="height: 60vh">
+
+          <div class="card d-grid h-100 mb-5 border-0 pt-0 ">
+
+            <div class="card-body align-self-center" style="font-family: YekanBakh; ">
+
+              <div class="register ">
+                <h5 class="text-justify">برای شرکت در گردونه شماره موبایلت رو وارد کن.
+                  هر روز فقط یک شانس داری پس اگر امروز شانس باهات یار نبود فردا بازم بیا!
+                </h5>
+                <label for="mobile">شماره موبایل</label>
+                <input type="text" id="mobile" v-model="mobile" class="form-control en mb-3 " placeholder="09...">
+                <button class="btn btn-success btn-sm" @click.prevent="register">دریافت کد تایید</button>
+                <ul class="mt-2">
+                  <li v-for="error in mobileErrors" id="mobileMessage" class="errorMessage small">{{ error }}</li>
+                </ul>
+              </div>
+              <div class="confirm d-none">
+                <small class="small">mobile</small>
+                <small class="small">لطفا کد تاییدی که از طریق پیامک دریافت کردید را وارد کنید:</small>
+                <div class="d-flex mb-3" dir="ltr">
+                  <input type="text" id="code1" @input="autoTab($event)" minLength="1" maxLength="1" min="0" max="9"
+                         class="form-control en text-center">
+                  <input type="text" id="code2" @input="autoTab($event)" minLength="1" maxLength="1" min="0" max="9"
+                         class="form-control en me-2 text-center">
+                  <input type="text" id="code3" @input="autoTab($event)" minLength="1" maxLength="1" min="0" max="9"
+                         class="form-control en me-2 text-center">
+                  <input type="text" id="code4" @input="autoTab($event)" minLength="1" maxLength="1" min="0" max="9"
+                         class="form-control en me-2 text-center">
+                </div>
+                <button class="btn btn-success btn-sm" @click="confirm">تایید</button>
+                <div class="col-12 d-flex justify-content-between mt-3 small ">
+                  <!--              <button  class="btn btn-sm btn-dark"  @click="getCode" >دریافت کد تایید</button>-->
+<!--                  <p @click="editNumber" class="text-info small cursor-pointer">-->
+<!--                    ویرایش شماره-->
+<!--                  </p>-->
+                  <p  id="resend" @click="resend" class=" disabledResend small">
+                    ارسال مجدد کد
+                    <span id="time">{{ time }}</span>
+                  </p>
+                </div>
+              </div>
+              <div class="luck d-none">
+                <h5>
+                  حالا روی گردونه کلیک کن تا جایزه ت رو ببری!
+                </h5>
+                <div class="row justify-content-center">
+                  <div class="col-lg-8">
+                    <div class="position-relative cursor-pointer text-center disabled-wheel " @click="play">
+                      <img src="/img/gb.png" class="img-fluid mx-auto mt-3 ww" width="80%"
+                           alt="">
+                      <img src="/img/bf.png" class="img-fluid mx-auto "
+                           style="position: absolute; top:-5px; left:39%"
+                           width="21%" alt="">
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+              <div class="result d-none text-justify" style="font-family: YekanBakhExtraBold;">
+                <h4>
+                  برنده شدی!
+                </h4>
+                <h5>
+                  با استفاده از کد تخفیف 4517894156 میتونی از این پلتفرم ها خرید کنی!
+                </h5>
+
+              </div>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+import {onMounted, ref} from "vue";
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    // HelloWorld
+  },
+
+  setup() {
+
+    onMounted(() => {
+      document.getElementById("mobile").value = '';
+      document.getElementById("code1").value = '';
+      document.getElementById("code2").value = '';
+      document.getElementById("code3").value = '';
+      document.getElementById("code4").value = '';
+      const myModal = document.getElementById('staticBackdrop')
+      const myInput = document.getElementById('mobile')
+
+      myModal.addEventListener('shown.bs.modal', () => {
+        myInput.focus()
+      })
+    });
+    const mobileErrors = ref([]);
+    const register = () => {
+      mobileErrors.value = [];
+      console.log(mobileErrors.value);
+      let mobile = document.querySelector('#mobile').value;
+      if (mobile.length == 0) {
+        mobileErrors.value.push('لطفا شماره موبایل خود را وارد کنید');
+      } else {
+        if (!mobile.startsWith('09')) {
+          mobileErrors.value.push('شماره موبایل باید با 09 آغاز شود.');
+        }
+        if (mobile.length != 11) {
+          mobileErrors.value.push('شماره موبایل باید 11 رقم باشد.');
+        }
+        if (mobile.startsWith('09') && mobile.length == 11) {
+          document.querySelector('.register').classList.add('d-none');
+          document.querySelector('.confirm').classList.remove('d-none');
+          counter();
+        }
+      }
+
+
+    }
+    const getCode = () => {
+      document.getElementById('resend').classList.add('disabledResend');
+      counter();
+
+        axios.post(apiUrl + '/api/otp/mobile', {
+          mobile: document.getElementById('mobile').value,
+        }, {progress: false})
+            .then((res) => {
+              if (res.status === 200) {
+                document.getElementById('mobileForm').classList.add('d-none');
+                document.getElementById('codeForm').classList.remove('d-none');
+              } else {
+                // message.value = res;
+                console.log(res)
+              }
+            })
+            .then(() => {
+              counter();
+                document.getElementById('resend').classList.add('disabledResend')
+            })
+            .catch((err) => {
+              message.value = err.response.data.message;
+            })
+      }
+
+
+
+    const apiUrl = ref('');
+    const errors = ref([]);
+    const message = ref([]);
+    const mobile = ref();
+    const time = ref(10);
+
+    const editNumber = () => {
+      mobile.value = '';
+      document.querySelector('.register').classList.remove('d-none');
+      document.querySelector('.confirm').classList.add('d-none');
+      document.getElementById('mobile').focus();
+      clearInterval();
+
+    }
+    const resend = () => {
+      getCode();
+    }
+    const counter = () => {
+
+      var distance = 10;
+      var x = null;
+      clearInterval(x);
+      time.value = 0;
+      x = setInterval(function () {
+        distance--;
+        time.value = distance;
+        var t = time.value < 10 ? "0" : "";
+        document.getElementById("time").innerHTML = t + time.value;
+
+        if (distance < 1) {
+          clearInterval(x);
+          // document.getElementById("time").classList.add('d-none');
+
+        }
+      }, 1000);
+
+      time.value = 0;
+      setTimeout(()=>{
+          document.getElementById('resend').classList.remove('disabledResend')
+
+      },11000);
+
+
+    }
+    const autoTab = (e) => {
+      let code =
+          document.getElementById("code1").value +
+          document.getElementById("code2").value +
+          document.getElementById("code3").value +
+          document.getElementById("code4").value;
+
+      if (code.length === 4) {
+
+        axios.post(apiUrl + '/api/mobile/login', {
+          mobile: document.getElementById('mobile').value,
+          scope: 'user',
+          password: document.getElementById("code1").value + document.getElementById("code2").value + document.getElementById("code3").value + document.getElementById("code4").value
+        })
+            .then((res) => {
+              if (res.status === 200) {
+                localStorage.setItem('user', JSON.stringify(res.data.user))
+                localStorage.setItem('token', JSON.stringify(res.data.access_token))
+                localStorage.setItem('scope', JSON.stringify(res.data.scope))
+                localStorage.setItem('expire', JSON.stringify(res.data.expire));
+                console.log(localStorage)
+                window.location = '/';
+              } else {
+                console.log(res)
+              }
+            }).catch((err) => {
+          message.value = err.response.data.message;
+        })
+
+      }
+
+      if (e.target.value.length === e.target.maxLength) {
+        e.target.nextElementSibling?.focus();
+      }
+    }
+    const confirm = () => {
+      document.querySelector('.confirm').classList.add('d-none');
+      document.querySelector('.luck').classList.remove('d-none');
+
+    }
+    const play = () => {
+      document.querySelector('.ww').classList.add('play');
+      setTimeout(() => {
+        document.querySelector('.luck').style.transform = 'scale(0,0)';
+      }, 7000);
+      setTimeout(() => {
+        document.querySelector('.luck').classList.add('d-none');
+        document.querySelector('.result').classList.remove('d-none');
+      }, 8000);
+      setTimeout(() => {
+        document.querySelector('.result').style.transform = 'scale(1,1)';
+      }, 8500);
+
+    }
+    return {
+      register, confirm, play, mobileErrors,
+      editNumber, getCode, resend, counter, autoTab,
+      mobile, message, time, errors
+    }
   }
 }
 </script>
